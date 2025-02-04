414176772945654f68c8a3dcb91e21e16640f2fd
Artyom WSL <artyomgv05@gmail.com>
2025-02-04 18:05:53 -0500

    initial commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Sidebar.scss';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <h2>Sidebar</h2>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/upload">Upload</Link></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
