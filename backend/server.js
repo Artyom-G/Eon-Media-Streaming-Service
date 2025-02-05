b25fcae85b19ed5257ca1d384c1898d126098060
Artyom WSL <artyomgv05@gmail.com>
2025-02-04 19:06:06 -0500

    backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const Video = require('./models/video');
+
 const app = express();
 const PORT = 5000;
 
@@ -16,6 +18,27 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .catch(err => {
     console.log('MongoDB connection error:', err);
   });
+
+app.get('/videos', async (req, res) => {
+    try {
+        const videos = await Video.find();
+        res.json(videos);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+app.get('/videos/:id', async (req, res) => {
+    try {
+        const video = await Video.findById(req.params.id);
+        if (!video) {
+        return res.status(404).json({ message: 'Video not found' });
+        }
+        res.json(video);
+    } catch (err) {
+        res.status(500).json({ message: 'Server error', error: err.message });
+    }
+});
 
 // Start the server
 app.listen(PORT, () => {
